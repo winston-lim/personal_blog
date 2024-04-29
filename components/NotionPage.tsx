6a0232a921f58af233477d525a90908f01b73ca5
winston-lim <winston_lim1@hotmail.com>
2024-04-29 18:10:59 +0800

    fix: disable search

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -22,7 +22,6 @@ import * as config from '@/lib/config'
 import * as types from '@/lib/types'
 import { mapImageUrl } from '@/lib/map-image-url'
 import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
-import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
@@ -311,7 +310,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         linkTableTitleProperties={false}
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapImageUrl}
-        searchNotion={config.isSearchEnabled ? searchNotion : null}
+        searchNotion={null}
         pageAside={pageAside}
         footer={footer}
         pageTitle={tagsPage && propertyToFilterName ? title : undefined}
